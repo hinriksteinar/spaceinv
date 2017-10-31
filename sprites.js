8253acf6283c525ba9f8b850b40c0c00f1c4eca1
Fatbastard1 <arnihl@gmail.com>
2017-10-31 16:25:43 +0000

    prufa

---
+++ sprites.js
@@ -0,0 +1,11 @@
+
+
+function Sprite(image){
+  this.width = image.width;
+  this.height = image.height;
+  this.scale = 1;
+}
+
+Sprite.prototype.drawAt = function (ctx, x, y){
+  ctx.drawImage(this.image,x,y);
+}
