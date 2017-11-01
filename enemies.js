9753a4d00ba3b88b5e8afb11286d940322b530d2
Fatbastard1 <arnihl@gmail.com>
2017-11-01 14:16:37 +0000

    bad guys 4 lyfe

---
+++ enemies.js
@@ -0,0 +1,21 @@
+//----------------------------------------------------------
+//  ENEMIES
+//
+//----------------------------------------------------------
+
+function Enemy(descr) {
+    for (var property in descr) {
+        this[property] = descr[property];
+    }
+}
+
+Enemy.prototype.render = function(ctx){
+  // STUFF
+  this.sprite.drawAt(ctx, this.cx,
+                          this.cy);
+}
+
+
+Enemy.prototype.update = function () {
+
+}
