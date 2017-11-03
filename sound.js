cb3737dda43572b78a015cdf881e0bda55c2f1b0
bjarnie3 <bjarnie3@msn.com>
2017-11-03 17:19:42 +0000

    Sound.js uppfærð
    Sound.js uppfærð og script includað í index skránni

--- sound.js
+++ sound.js
@@ -3,23 +3,69 @@
 // ===========
 
 var sounds = {
-  "bump2": {
+  "backgroundsound": {
+    url : '../Sound/X-Files-Illuminati-Song.mp3',
+    volume : .05
+  },
+  "bomb": {
+    url : '../Sound/explosion.wav',
+    volume : .05
+  },
+    "bomb2": {
+    url : '../Sound/explosion2.wav',
+    volume : .05
+  },
+    "invaderkilled": {
+    url : '../Sound/invaderkilled.wav',
+    volume : .05
+  },
+    "shoot": {
+    url : '../Sound/shoot.wav',
+    volume : .05
+  },
+    "ufo_highpitch": {
+    url : '../Sound/ufo_highpitch.wav',
+    volume : .05
+  },
+    "ufo_lowpitch": {
+    url : '../Sound/ufo_lowpitch.wav',
+    volume : .05
+  },
+    "fastinvader": {
+    url : '../Sound/fastinvader.wav',
+    volume : .05
+  },
+    "fastinvader": {
+    url : '../Sound/fastinvader2.wav',
+    volume : .05
+  },
+    "fastinvader3": {
+    url : '../Sound/fastinvader.wav',
+    volume : .05
+  },
+    "fastinvader4": {
+    url : '../Sound/fastinvader.wav',
+    volume : .05
+  },
+  {
+    "bump": {
     url : "https://freesound.org/data/previews/118/118649_2139651-lq.mp3",
     volume : .05
-  }
+  }}
 };
 
+// kalla úr annari skrá t.d playSound('bomb'); sem dæmi
 
 var soundContext = new AudioContext();
 
 for(var key in sounds) {
   loadSound(key);
 }
-
 function loadSound(name){
   var sound = sounds[name];
-
   var url = sound.url;
+  var sound1 = sounds1[name];
+  var url1 = sound1.url;
   var buffer = sound.buffer;
 
   var request = new XMLHttpRequest();
